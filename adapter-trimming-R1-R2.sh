8400bbfb554e9438533a349bc826e2fd7f9fe05f
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-12-19 15:32:39 -0800

    Update adapter-trimming-R1-R2.sh

--- adapter-trimming-R1-R2.sh
+++ adapter-trimming-R1-R2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+#SBATCH --account=fc_rnaseq
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=enrico_calvane@berkeley.edu
+#SBATCH --mail-type=ALL
 # Create output directory for processed files
 mkdir -p processed_fastx
 
