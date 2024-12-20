fd1a1c7eb3ae1208183367f7ffac9cf267f7b372
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-12-20 12:33:08 -0800

    Update sorting_ribotish_qc.sh

--- sorting_ribotish_qc.sh
+++ sorting_ribotish_qc.sh
@@ -7,7 +7,11 @@
 #SBATCH --time=06:00:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
- 
+
+# Directory variables for cleaner code
+UNIQUE_READS="/global/scratch/users/enricocalvane/riboseq/imb2/unique_reads"
+REF_DIR="/global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference"
+
 cd /global/scratch/users/enricocalvane/riboseq/imb2
 
 cd unique_reads
@@ -15,24 +19,38 @@ cd unique_reads
 module load bio/samtools/1.17-gcc-11.4.0
 
 # First, sort all BAM files
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-1_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-1_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-2_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-2_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-1_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-1_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-2_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-2_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-1_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-1_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-2_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-2_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-1_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-1_uniq_sort.bam
-samtools sort /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-2_uniq.bam -o /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-2_uniq_sort.bam
+echo "Sorting BAM files..."
+samtools sort ${UNIQUE_READS}/LZT101-1_uniq.bam -o ${UNIQUE_READS}/LZT101-1_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT101-2_uniq.bam -o ${UNIQUE_READS}/LZT101-2_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT102-1_uniq.bam -o ${UNIQUE_READS}/LZT102-1_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT102-2_uniq.bam -o ${UNIQUE_READS}/LZT102-2_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT103-1_uniq.bam -o ${UNIQUE_READS}/LZT103-1_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT103-2_uniq.bam -o ${UNIQUE_READS}/LZT103-2_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT104-1_uniq.bam -o ${UNIQUE_READS}/LZT104-1_uniq_sort.bam
+samtools sort ${UNIQUE_READS}/LZT104-2_uniq.bam -o ${UNIQUE_READS}/LZT104-2_uniq_sort.bam
+
+# Index all sorted BAM files
+echo "Indexing sorted BAM files..."
+samtools index ${UNIQUE_READS}/LZT101-1_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT101-2_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT102-1_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT102-2_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT103-1_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT103-2_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT104-1_uniq_sort.bam
+samtools index ${UNIQUE_READS}/LZT104-2_uniq_sort.bam
 
 # Convert GFF3 to GTF
-gffread /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gff3 -T -o /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf
+echo "Converting GFF3 to GTF..."
+gffread ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gff3 -T -o ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf
 
 # Run Ribotish quality for each sorted BAM file
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-1_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-1_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT101-2_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-2_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-1_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-1_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT102-2_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-2_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-1_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-1_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT103-2_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-2_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-1_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-1_quality.txt
-ribotish quality -b /global/scratch/users/enricocalvane/riboseq/imb2/unique_reads/LZT104-2_uniq_sort.bam -g /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-2_quality.txt
+echo "Running Ribotish quality control..."
+ribotish quality -b ${UNIQUE_READS}/LZT101-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-1_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT101-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-2_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT102-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-1_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT102-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-2_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT103-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-1_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT103-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-2_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT104-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-1_quality.txt
+ribotish quality -b ${UNIQUE_READS}/LZT104-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-2_quality.txt
