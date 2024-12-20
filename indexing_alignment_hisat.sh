1fe53c5f98b42e0594fd9861af46ee84be58bac9
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-12-19 23:16:35 -0800

    Update indexing_alignment_hisat.sh

--- indexing_alignment_hisat.sh
+++ indexing_alignment_hisat.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#SBATCH --account=fc_rnaseq
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=enrico_calvane@berkeley.edu
+#SBATCH --mail-type=ALL
 
 # Set variables
 REFERENCE_DIR="/global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference"
