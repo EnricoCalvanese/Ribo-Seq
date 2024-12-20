16c9ee1c44a57d44ef5a51e1290767cdc79f9fbe
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-12-20 13:10:29 -0800

    Update sorting_ribotish_qc.sh

--- sorting_ribotish_qc.sh
+++ sorting_ribotish_qc.sh
@@ -18,23 +18,15 @@ cd unique_reads
 
 module load bio/samtools/1.17-gcc-11.4.0
 
-# First, sort all BAM files
-echo "Sorting BAM files..."
-samtools sort ${UNIQUE_READS}/LZT101-1_uniq.bam -o ${UNIQUE_READS}/LZT101-1_uniq_sort.bam
-samtools sort ${UNIQUE_READS}/LZT101-2_uniq.bam -o ${UNIQUE_READS}/LZT101-2_uniq_sort.bam
-samtools sort ${UNIQUE_READS}/LZT102-1_uniq.bam -o ${UNIQUE_READS}/LZT102-1_uniq_sort.bam
-samtools sort ${UNIQUE_READS}/LZT102-2_uniq.bam -o ${UNIQUE_READS}/LZT102-2_uniq_sort.bam
+# Sort only Ribo-seq BAM files
+echo "Sorting Ribo-seq BAM files..."
 samtools sort ${UNIQUE_READS}/LZT103-1_uniq.bam -o ${UNIQUE_READS}/LZT103-1_uniq_sort.bam
 samtools sort ${UNIQUE_READS}/LZT103-2_uniq.bam -o ${UNIQUE_READS}/LZT103-2_uniq_sort.bam
 samtools sort ${UNIQUE_READS}/LZT104-1_uniq.bam -o ${UNIQUE_READS}/LZT104-1_uniq_sort.bam
 samtools sort ${UNIQUE_READS}/LZT104-2_uniq.bam -o ${UNIQUE_READS}/LZT104-2_uniq_sort.bam
 
-# Index all sorted BAM files
-echo "Indexing sorted BAM files..."
-samtools index ${UNIQUE_READS}/LZT101-1_uniq_sort.bam
-samtools index ${UNIQUE_READS}/LZT101-2_uniq_sort.bam
-samtools index ${UNIQUE_READS}/LZT102-1_uniq_sort.bam
-samtools index ${UNIQUE_READS}/LZT102-2_uniq_sort.bam
+# Index sorted Ribo-seq BAM files
+echo "Indexing sorted Ribo-seq BAM files..."
 samtools index ${UNIQUE_READS}/LZT103-1_uniq_sort.bam
 samtools index ${UNIQUE_READS}/LZT103-2_uniq_sort.bam
 samtools index ${UNIQUE_READS}/LZT104-1_uniq_sort.bam
@@ -44,13 +36,18 @@ samtools index ${UNIQUE_READS}/LZT104-2_uniq_sort.bam
 echo "Converting GFF3 to GTF..."
 gffread ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gff3 -T -o ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf
 
-# Run Ribotish quality for each sorted BAM file
-echo "Running Ribotish quality control..."
-ribotish quality -b ${UNIQUE_READS}/LZT101-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-1_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT101-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT101-2_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT102-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-1_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT102-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT102-2_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT103-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-1_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT103-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT103-2_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT104-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-1_quality.txt
-ribotish quality -b ${UNIQUE_READS}/LZT104-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > LZT104-2_quality.txt
+# Run Ribotish quality for Ribo-seq samples only
+echo "Running Ribotish quality control on Ribo-seq samples..."
+echo "Processing WT Ribo-seq Rep1 (LZT103-1)..."
+ribotish quality -b ${UNIQUE_READS}/LZT103-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > RF_WT_Rep1_quality.txt
+
+echo "Processing WT Ribo-seq Rep2 (LZT103-2)..."
+ribotish quality -b ${UNIQUE_READS}/LZT103-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > RF_WT_Rep2_quality.txt
+
+echo "Processing imb2 Ribo-seq Rep1 (LZT104-1)..."
+ribotish quality -b ${UNIQUE_READS}/LZT104-1_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > RF_imb2_Rep1_quality.txt
+
+echo "Processing imb2 Ribo-seq Rep2 (LZT104-2)..."
+ribotish quality -b ${UNIQUE_READS}/LZT104-2_uniq_sort.bam -g ${REF_DIR}/Arabidopsis_thaliana.TAIR10.60.gtf > RF_imb2_Rep2_quality.txt
+
+echo "Quality control analysis complete!"
