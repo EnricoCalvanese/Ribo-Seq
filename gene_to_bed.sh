247a4c3f0ce9e4cb5e3072d3ee90f4af4c3f2bc7
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-12-23 13:35:04 -0800

    Update gene_to_bed.sh

--- gene_to_bed.sh
+++ gene_to_bed.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#SBATCH --account=fc_rnaseq
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=enrico_calvane@berkeley.edu
+#SBATCH --mail-type=ALL
 
+# Create BED files
+python gene_to_bed.py TEup_genes.txt /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf TEup_genes.bed
+
+python gene_to_bed.py TEdown_genes.txt /global/scratch/users/enricocalvane/riboseq/Xu2017/tair10_reference/Arabidopsis_thaliana.TAIR10.60.gtf TEdown_genes.bed
