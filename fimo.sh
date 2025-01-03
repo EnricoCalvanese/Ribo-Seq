c32f71538905566c3368001c85ea48a007ab1fcd
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-01-03 14:51:32 -0800

    Update fimo.sh

--- fimo.sh
+++ fimo.sh
@@ -4,10 +4,26 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=06:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
+#SBATCH --array=1-7
+#SBATCH --job-name=seq_prep
+#SBATCH --output=seq_prep_%A_%a.out
+#SBATCH --error=seq_prep_%A_%a.err
 
 cd /global/scratch/users/enricocalvane/riboseq/imb2/FIMO
 
-/global/home/users/enricocalvane/.conda/envs/riboseq/bin/python prepare_for_FIMO.py
+# Map array index to category
+case $SLURM_ARRAY_TASK_ID in
+    1) category="uorf_down" ;;
+    2) category="uorf_up" ;;
+    3) category="uorf_nc" ;;
+    4) category="translatome_down" ;;
+    5) category="translatome_up" ;;
+    6) category="translatome_nc" ;;
+    7) category="full_transcriptome" ;;
+esac
+
+# Run the script for this category
+/global/home/users/enricocalvane/.conda/envs/riboseq/bin/python prepare_for_MEME-FIMO.py $category
