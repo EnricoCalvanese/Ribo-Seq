ddce11a3a0a15eb24a2e22b6dacdf9e31dd01660
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-07-01 17:17:50 -0700

    Update prepare_transcripts.sh

--- ribominer/prepare_transcripts.sh
+++ ribominer/prepare_transcripts.sh
@@ -16,16 +16,31 @@ date
 
 # Paths
 GTF_ORIG="/global/scratch/users/enricocalvane/riboseq/Athaliana_447_Araport11.gene.gtf"
+GTF_PATCHED="/global/scratch/users/enricocalvane/riboseq/Athaliana_447_Araport11.gene.patched.gtf"
 FASTA="/global/scratch/users/enricocalvane/riboseq/Athaliana_447_TAIR10.fa"
 OUTDIR="/global/scratch/users/enricocalvane/riboseq/metagene_plot_ribominer/prepared_transcripts"
 SIF="ribocode_ribominer_latest.sif"
 
 mkdir -p "$OUTDIR"
 
+awk '
+BEGIN { OFS = "\t" }
+($3 == "transcript" || $3 == "exon" || $3 == "CDS") {
+  tid = ""; gid = ""; rest = "";
+  match($9, /transcript_id "([^"]+)"/, t);
+  match($9, /gene_id "([^"]+)"/, g);
+  if (t[1] && g[1]) {
+    tid = t[1]; gid = g[1];
+    $9 = "transcript_id \"" tid "\"; gene_id \"" gid "\"; transcript_biotype \"protein_coding\";";
+    print;
+  }
+}
+' "$GTF_ORIG" > "$GTF_PATCHED"
+
 echo "Running prepare_transcripts with Singularity..."
 singularity exec "$SIF" \
   /root/miniconda3/bin/prepare_transcripts \
-  -g "$GTF_ORIG" \
+  -g "$GTF_PATCHED" \
   -f "$FASTA" \
   -o "$OUTDIR"
 
