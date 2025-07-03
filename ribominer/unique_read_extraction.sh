8edd225000d3533d72520393fb647fa0d8adbf10
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-07-03 14:23:19 -0700

    Update unique_read_extraction.sh

--- ribominer/unique_read_extraction.sh
+++ ribominer/unique_read_extraction.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=03:00:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=indexing_alignment_hisat.log
+#SBATCH --output=unique_read_extraction.log
 
 cd /global/scratch/users/enricocalvane/riboseq/metagene_plot_ribominer/reads
 
