08cdea23a7b4decbf107e04dbdd904edddd0b428
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-07-03 14:20:22 -0700

    Update unique_read_extraction.sh

--- ribominer/unique_read_extraction.sh
+++ ribominer/unique_read_extraction.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+#SBATCH --account=fc_rnaseq
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=03:00:00
+#SBATCH --mail-user=enrico_calvane@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=indexing_alignment_hisat.log
