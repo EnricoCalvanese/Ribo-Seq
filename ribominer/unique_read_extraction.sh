858e53f6b19ed7435f96aec8ee291e7418e86e6f
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-07-03 14:41:51 -0700

    Update unique_read_extraction.sh

--- ribominer/unique_read_extraction.sh
+++ ribominer/unique_read_extraction.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=unique_read_extraction.log
