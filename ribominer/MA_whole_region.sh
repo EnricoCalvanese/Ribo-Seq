3ab2ee056cd03f5ae518bcdffd8f0c3a0e24364e
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-07-12 16:42:21 -0700

    Update MA_whole_region.sh

--- ribominer/MA_whole_region.sh
+++ ribominer/MA_whole_region.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=metagene_analysis_whole_region.log
+#SBATCH --output=metagene_analysis_allgenes.log
 
 # Define variables
 WORK_DIR="/global/scratch/users/enricocalvane/riboseq/metagene_plot_ribominer"
